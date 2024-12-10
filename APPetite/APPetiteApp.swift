e1983229e5e9f4f35080ba69b495f768e07c83d5
Felix Krumme <Felix.krumme@t-online.de>
2024-12-10 15:04:38 -0800

    Initial Commit

---
+++ APPetite/APPetiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPetiteApp.swift
+//  APPetite
+//
+//  Created by Felix Krumme on 10.12.24.
+//
+
+import SwiftUI
+
+@main
+struct APPetiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APPetiteTests/APPetiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  APPetiteTests.swift
+//  APPetiteTests
+//
+//  Created by Felix Krumme on 10.12.24.
+//
+
+import Testing
+@testable import APPetite
+
+struct APPetiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
