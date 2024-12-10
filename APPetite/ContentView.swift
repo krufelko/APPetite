7ac038a03d80d524f90c7863935c0a14dd0b0d24
Felix Krumme <Felix.krumme@t-online.de>
2024-12-10 15:19:16 -0800

    Added Text("Hello can you see this?") in content View to test

--- APPetite/ContentView.swift
+++ APPetite/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, woorld!")
+            Text("Hello can you see this?")
         }
         .padding()
     }
