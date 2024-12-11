4c3f8a4768254a3654bcfc40f710af6f35501033
Felix Krumme <Felix.krumme@t-online.de>
2024-12-11 14:43:58 -0800

    Added RecipeBookmarkManager and edited BookmarksView

---
+++ APPetite/Services/RecipeBookMarkManager.swift
@@ -0,0 +1,64 @@
+//
+//  RecipeBookMarkManager.swift
+//  APPetite
+//
+//  Created by Felix Krumme on 11.12.24.
+//
+
+//
+//  Recipe.swift
+//  Appetite
+//
+//  Created by Evelyn Tran on 11/16/24.
+//
+
+import Foundation
+
+class RecipeBookmarkManager {
+    private let bookmarksKey = "bookmarkedRecipes"
+    private var bookmarks: [Recipe] = []
+
+    init() {
+        loadBookmarks()
+    }
+
+    func loadBookmarks() {
+        if let data = UserDefaults.standard.data(forKey: bookmarksKey) {
+            do {
+                let decodedRecipes = try JSONDecoder().decode([Recipe].self, from: data)
+                bookmarks = decodedRecipes
+            } catch {
+                print("Failed to decode bookmarks: \(error)")
+            }
+        }
+    }
+
+    func saveBookmarks() {
+        do {
+            let data = try JSONEncoder().encode(bookmarks)
+            UserDefaults.standard.set(data, forKey: bookmarksKey)
+        } catch {
+            print("Failed to save bookmarks: \(error)")
+        }
+    }
+
+    func addBookmark(_ recipe: Recipe) {
+        if !bookmarks.contains(where: { $0.id == recipe.id }) {
+            bookmarks.append(recipe)
+            saveBookmarks()
+        }
+    }
+
+    func removeBookmark(_ recipe: Recipe) {
+        bookmarks.removeAll(where: { $0.id == recipe.id })
+        saveBookmarks()
+    }
+
+    func isBookmarked(_ recipe: Recipe) -> Bool {
+        return bookmarks.contains(where: { $0.id == recipe.id })
+    }
+
+    func getAllBookmarks() -> [Recipe] {
+        return bookmarks
+    }
+}
--- APPetite/Views/BookmarksView.swift
+++ APPetite/Views/BookmarksView.swift
@@ -1,14 +1,45 @@
 //
-//  Untitled.swift
-//  APPetite
+//  BookmarkView.swift
+//  Appetite
 //
-//  Created by Felix Krumme on 11.12.24.
+//  Created by Felix Krumme on 11/12/24.
 //
 
 import SwiftUI
 
-struct BookmarksView: View {
+struct BookmarkView: View {
+    @StateObject private var bookmarkManager = RecipeBookmarkManager()
+
     var body: some View {
-        Text("Bookmarks")
+        NavigationView {
+            List {
+                ForEach(bookmarkManager.getAllBookmarks().sorted(by: { $0.name < $1.name })) { recipe in
+                    NavigationLink(destination: RecipeCardView(recipe: recipe)) {
+                        Text(recipe.name)
+                            .font(.headline)
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .navigationTitle("Bookmarked Recipes")
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+
+    private func deleteItems(at offsets: IndexSet) {
+        let sortedBookmarks = bookmarkManager.getAllBookmarks().sorted(by: { $0.name < $1.name })
+        offsets.forEach { index in
+            let recipeToDelete = sortedBookmarks[index]
+            bookmarkManager.removeBookmark(recipeToDelete)
+        }
     }
 }
+
+struct BookmarkView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookmarkView()
+    }
+}
+
